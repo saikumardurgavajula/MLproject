6cfdaa9eed99d37c60a8b0dd40738c1d51481e36
saikumardurgavajula <saikumarloveyou3000@gmail.com>
2025-05-26 11:08:58 +0530

    Project structure

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+from src.logger import logging
 
 def error_message_details(error, error_detail: sys):
     """
@@ -25,5 +26,7 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+
+
 
 
